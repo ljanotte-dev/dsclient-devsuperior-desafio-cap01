b8a93712547a3abcc44a07f49c6b32058c5f95d3
ljanotte.dev <ljanotte-dev@gmail.com>
2022-06-05 22:07:13 -0300

    Client, ClientResource

---
+++ dsclient/backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsclient.resources;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsclient.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = new ArrayList<>();
+		list.add(new Client(1L, "Leandro", "034246599.67", 1200.00, Instant.parse("2018-12-30T19:34:50.63Z"), 1));
+		return ResponseEntity.ok().body(list);
+	}
+}
