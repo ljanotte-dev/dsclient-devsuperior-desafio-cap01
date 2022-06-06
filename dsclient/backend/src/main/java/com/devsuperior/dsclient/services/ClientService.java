54e0872bb95b19a45a248de1b70f3afb1a3073e7
ljanotte.dev <ljanotte-dev@gmail.com>
2022-06-05 23:40:37 -0300

    Find Client by id, exception handling

--- dsclient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ dsclient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,6 +24,13 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		List<ClientDTO> listDto = list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 		return listDto;
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 	
 }
